2efe6ad6dd0330e1148a9927ed4061b26e8a1fcb
jiudi <php8082@163.com>
2017-02-26 12:24:17 +0800

    初始化代码下

---
+++ application/install/config.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+
+	'view_replace_str' => array(
+		'__ADDONS__'           => BASE_PATH . '/addons',
+		'__PUBLIC__'           => BASE_PATH . '/public',
+		'__STATIC__'           => BASE_PATH . '/application/admin/static',
+		'__IMG__'              => BASE_PATH . '/application/admin/static/images',
+		'__CSS__'              => BASE_PATH . '/application/admin/static/css',
+		'__JS__'               => BASE_PATH . '/application/admin/static/js',
+		'__COMPANY__'          => '上海摄影哈哈哈哈',
+		'__NAME__'             => 'PhotoArt系统',
+		'__COMPANY_WEBSITE__' => 'github.com',
+		'__WEBSITE__' => 'github.com',
+	),
+);
